c8d1938850ede786ef780bb6e5ab69ef7c1873b3
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-03-13 16:07:46 -0300

    feat: :lipstick: Build UI for Calculadora

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import { useState } from 'react'
 import './App.css'
 
+import logoImg from './assets/logo.png'
+
 function App() {
 
   return (
     <div>
-      <h1>TESTE</h1>
+      <main className='container'>
+        <img
+          className='logo'
+          src={logoImg} 
+          alt="Logo da calculadora de gasolina ou alcool" 
+        />
+        <h1 className='title'>Qual a Melhor Opção?</h1>
+      
+        <form className='form'>
+          <label>Álcool (preço por litro):</label>
+          <input 
+            className='input'
+            type="number"
+            placeholder='4,50'
+            min="1"
+            step="0.01"
+            required 
+          />
+
+          <label>Gasolina (preço por litro):</label>
+          <input 
+            className='input'
+            type="number"
+            placeholder='4,50'
+            min="1"
+            step="0.01"
+            required 
+          />
+
+          <input className='button' type="button" value="Calcular" />
+        </form>
+      </main>
     </div>
   )
 }
